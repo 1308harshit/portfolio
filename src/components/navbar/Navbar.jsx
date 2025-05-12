49a16c994f1d217e53a8ae6d4236ac9308412286
Harshit Khatsuriya <139879303+1308harshit@users.noreply.github.com>
2025-05-12 19:55:01 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
       <div className="wrapper">
         <motion.span initial={{opacity:0, scale:0.5}}  animate={{opacity:1, scale:1}} transition={{duration:0.5}}>Welcome!</motion.span>
         <div className="social">
-          <a href="https://drive.google.com/file/d/1pZGmeQeS5oyY0bzHCov_ljg2R9rdIkFS/view?usp=sharing" target='_blank'><img src="/resume2.png" alt="" target='_blank'/></a> 
+          <a href="https://drive.google.com/file/d/1PphMXr9b_rM7aavcAEt9jHOtwwwqorCa/view?usp=sharing" target='_blank'><img src="/resume2.png" alt="" target='_blank'/></a> 
           <a href="https://www.linkedin.com/in/harshit-khatsuriya-13a607274/" target='_blank'><img src="/linkedIn.png"  alt="LinkedIn" /></a> {/* Linked In  */}
           <a href="https://github.com/1308harshit"><img src="/github.png" alt="Github" target='_blank'/></a> {/* Github  */}
           {/* <a href="#"><img src="/instagram.png" alt="" target='_blank'/></a>  Email  */}
